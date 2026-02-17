e9327d19261e5c517ec78d0ce13cf8b4fc48a593
TGVM <tgvm96@gmail.com>
2026-02-17 14:14:52 -0300

    Added extra controller functions

--- MPKart/Assets/Scripts/Input/KartController.cs
+++ MPKart/Assets/Scripts/Input/KartController.cs
@@ -26,19 +26,48 @@ namespace Kart
 
         [Header("Steering Attributes")]
         [SerializeField] float maxSteeringAngle = 30f;
+        [SerializeField] AnimationCurve turnCurve;
+        [SerializeField] float turnStrenght = 1500f;
 
         [Header("Braking and Drifting Attributes")]
+        [SerializeField] float driftSteerMultiplier = 1.5f;
         [SerializeField] float brakeTorque = 10000f;
-        float brakeVelocity;
 
+        [Header("Physics")]
+        [SerializeField] Transform centerOfMass;
+        [SerializeField] float downForce = 100f;
+        [SerializeField] float gravity = Physics.gravity.y;
+        [SerializeField] float lateralGScale = 10f;
 
+        [Header("Banking")]
+        [SerializeField] float maxBankAngle = 5f;
+        [SerializeField] float bankSpeed = 2f;
+
+        [Header("Refs")]
         [SerializeField] InputReader input;
         Rigidbody rb;
+
+        Vector3 kartVelocity;
+        float brakeVelocity;
+        float driftVelocity;
+
+        RaycastHit hit;
+
+        const float thresholdSpeed = 10f;
+        const float centerOfMassOffset = -0.5f;
+        Vector3 originalCenterOfMass;
+
+        public bool IsGrounded = true;
+        public Vector3 Velocity => kartVelocity;
+        public float MaxSpeed => maxSpeed;
 
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
             input.Enable();
+
+            rb.centerOfMass = centerOfMass.localPosition;
+            originalCenterOfMass = centerOfMass.localPosition;
 
             foreach (AxleInfo axleInfo in axleInfos) {
                 axleInfo.originalForwardFriction = axleInfo.leftWheel.forwardFriction;
@@ -55,13 +84,73 @@ namespace Kart
             float motor = maxMotorTorque * verticalInput;
             float steering = maxSteeringAngle * horizontalInput;
 
-            Debug.Log("Motor: " + motor);
-            Debug.Log("Steering: " + steering);
-            Debug.Log("Vertical Input: " + verticalInput);
-            Debug.Log("Horizontal Input: " + horizontalInput);
+            //Debug.Log("Motor: " + motor);
+            //Debug.Log("Steering: " + steering);
+            //Debug.Log("Vertical Input: " + verticalInput);
+            //Debug.Log("Horizontal Input: " + horizontalInput);
 
 
             UpdateAxles(motor, steering);
+            UpdateBanking(horizontalInput);
+
+            kartVelocity = transform.InverseTransformDirection(rb.linearVelocity);//uses linearVelocity
+
+            if (IsGrounded)
+            {
+                HandleGroundedMovement(verticalInput, horizontalInput);
+            }
+            else
+            {
+                HandleAirborneMovement(verticalInput, horizontalInput);
+            }
+        }
+
+        private void HandleAirborneMovement(float verticalInput, float horizontalInput)
+        {
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, rb.linearVelocity + Vector3.down * gravity, Time.deltaTime * gravity);
+        }
+
+        private void HandleGroundedMovement(float verticalInput, float horizontalInput)
+        {
+            //turn logic
+            if(Mathf.Abs(verticalInput) > 0.1f || Mathf.Abs(kartVelocity.z) > 1)
+            {
+                float turnMultiplier = Mathf.Clamp01(turnCurve.Evaluate(kartVelocity.magnitude / maxSpeed));
+                rb.AddTorque(Vector3.up * horizontalInput * Mathf.Sign(kartVelocity.z) * turnStrenght * 100f * turnMultiplier);
+            }
+
+            //Acceleration logic
+            if (!input.IsBraking) {
+                float targetSpeed = verticalInput * maxSpeed;
+                Vector3 forwardWithoutY = new Vector3(transform.forward.x,
+                    0,
+                    transform.forward.z).normalized;
+                rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, forwardWithoutY * targetSpeed, Time.deltaTime);              
+            }
+
+            //Downforce
+            float speedFactor = Mathf.Clamp01(rb.linearVelocity.magnitude / maxSpeed);
+            float lateralG = Mathf.Abs(Vector3.Dot(rb.linearVelocity, transform.right));
+            float downforceFactor = Mathf.Max(speedFactor, lateralG / lateralGScale);
+            rb.AddForce(-transform.up * downForce * rb.mass * downforceFactor);
+
+            //Shift Center of Mass
+            float speed = rb.linearVelocity.magnitude;
+            Vector3 centerOfMassAdjustment = (speed > thresholdSpeed)
+                ? new Vector3(0f,0f,Mathf.Abs(verticalInput) > 0.1f ?
+                    Mathf.Sign(verticalInput) * centerOfMassOffset
+                    : 0f)
+                : Vector3.zero;
+
+            rb.centerOfMass = originalCenterOfMass + centerOfMassAdjustment;
+        }
+
+        private void UpdateBanking(float horizontalInput)
+        {
+            float targetBankAngle = horizontalInput * -maxBankAngle;
+            Vector3 currentEuler = transform.localEulerAngles;
+            currentEuler.z = Mathf.LerpAngle(currentEuler.z, targetBankAngle, Time.deltaTime * bankSpeed);
+            transform.localEulerAngles = currentEuler;
         }
 
         private void UpdateAxles(float motor, float steering)
